9a119b660c7e4c08f556224d1a3ebefceffd0dbf
Eduardo Barros de Souza <eduardobarros.biologia@gmail.com>
2025-09-17 12:12:34 -0300

    add atualizado

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-    // pegar a tag img
+  // pegar a tag img
   const img = document.querySelector("#profile img")
-    // substituir a img
+
+  // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/leão.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/pantera.preta.png")
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
   }
-
-  //if (html.classList.contains("light")) {
-  //  html.classList.remove("light")
-  //} else {
-  // html.classList.add("light")
-  //}
- 
 }
